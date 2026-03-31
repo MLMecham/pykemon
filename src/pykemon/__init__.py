ff6d120b3fcfbfe1df59de5302a8b22b9b4880a8
MLMecham <mec21017@byui.edu>
2026-03-31 15:12:40 -0600

    exported get_connection

--- src/pykemon/__init__.py
+++ src/pykemon/__init__.py
@@ -1,7 +1,7 @@
-# read version from installed package
 from importlib.metadata import version
 
 __version__ = version("pykemon")
 
-
 from pykemon.CRUD.team_editor import team_editor
+
+from pykemon.db import get_connection
